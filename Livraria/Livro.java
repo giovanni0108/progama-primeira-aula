d463746b4f50e9f97420815757d9e0b29f70cb10
giovanni0108 <giovannibb2030@gmail.com>
2024-08-26 14:33:17 +0000

    livraria

---
+++ Livraria/Livro.java
@@ -0,0 +1,18 @@
+package Livraria;
+
+public class Livro {
+    String titulo;
+    int numeropagina;
+    double preco;
+
+    void cadastrar(){
+        System.out.printLn("cadastrando o livro")
+    }
+
+    double calcFrete(String cep){
+        double frete = 0;
+        System.out.printLn(" calculando o frete paqra o cep+ cep");
+        return fret;
+    }
+    
+}
